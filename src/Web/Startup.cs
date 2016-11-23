d864e8050b602739d812ac366a8852aca152b965
SR <sunny@sanelib.com>
2016-11-23 10:41:56 +0530

    Use IIS integration for running in IISEXPRESS.

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -40,6 +40,7 @@ namespace DotNetData
             loggerFactory.AddConsole();
             loggerFactory.AddDebug();
 
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseMvc();
@@ -48,6 +49,7 @@ namespace DotNetData
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseIISIntegration()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
